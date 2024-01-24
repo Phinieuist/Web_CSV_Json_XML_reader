98cac7e782233b15ea2b38a14d56de02552c9106
Павел <kpb121@mail.ru>
2024-01-24 23:32:52 +0300

    Добавлена база данных и авторизация через куки

---
+++ Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_CSV_Json_XML_reader.Entities
+{
+    public class User
+    {
+
+        [Key]
+        public Guid UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+
+        public User() { }
+
+        public User(Guid UserId, string Email, string Password)
+        {
+            this.UserId = UserId;
+            this.Email = Email;
+            this.Password = Password;
+        }
+    }
+}
---
+++ Managers/IUserManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.Metrics;
+using Web_CSV_Json_XML_reader.Entities;
+
+namespace Web_CSV_Json_XML_reader.Managers
+{
+    public interface IUserManager
+    {
+        public Task<IReadOnlyCollection<User>> GetUsers();
+
+        public Task<User> GetUser(string email, string password);
+
+        public Task<bool> AddUser(User user);
+
+        public Task<bool> AddUser(string email, string password);
+    }
+}
